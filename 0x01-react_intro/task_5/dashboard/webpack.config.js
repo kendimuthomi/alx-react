515b80ce93381bdb8e4be448b7f6393f905d734d
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-05-08 09:37:27 +0300

    Create a project using Webpack

---
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    hot: true,
+  },
+};
