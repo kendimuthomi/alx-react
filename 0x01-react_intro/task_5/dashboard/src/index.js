91cbbaef9be09141c1dde48a6d370dd45a7579da
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-05-08 10:44:03 +0300

    Create a project using Webpack

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App/App";
-import Notifications from "./Notifications/Notifications";
+import './styles.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div id="root-notifications">
-      <Notifications />
-    </div>
-    <App />
-  </React.StrictMode>,
   document.getElementById("root")
 );
