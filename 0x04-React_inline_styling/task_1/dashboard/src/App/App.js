3aaa912702f7ce033ed8fb98c30df5d012f67480
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-05-18 12:07:16 +0300

    React inline styling using aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
@@ -81,6 +81,14 @@ const styles = StyleSheet.create({
     position: "relative",
     fontFamily: "Arial, Helvetica, sans-serif",
   },
+
+  Footer: {
+    fontSize: "1rem",
+    padding: "1.2rem",
+    textAlign: "center",
+    fontStyle: "italic",
+    borderTop: "3px solid #e0354b",
+  },
 });
 
 App.defaultProps = {
